dded86f05e8e81ad33e10bf693cde7cb77030b50
brandon <bresheske@gmail.com>
2020-03-14 08:14:46 -0500

    working filters

--- entities/apple/behaviors/color/red.ts
+++ entities/apple/behaviors/color/red.ts
@@ -1,10 +1,13 @@
-import { Apple } from "../../model";
+import { Apple, Color } from "../../model";
 
 /**
  * when an apple is red, we do something special when
  * the apple is created as a seed.
  */
 export async function onSeed(apple: Apple): Promise<boolean> {
+    if (apple.color !== Color.Red)
+        return true;
+
     console.log(`a red apple was created!!`);
     return true;
 }
@@ -14,6 +17,9 @@ export async function onSeed(apple: Apple): Promise<boolean> {
  * the apple becomes rotten
  */
 export async function onRotten(apple: Apple): Promise<boolean> {
+    if (apple.color !== Color.Red)
+        return true;
+
     console.log(`a red apple has rotted.`);
     return true;
 }
--- entities/apple/behaviors/sweetness/sweet.ts
+++ entities/apple/behaviors/sweetness/sweet.ts
@@ -1,6 +1,9 @@
-import { Apple } from "../../model";
+import { Apple, Sweetness } from "../../model";
 
 export async function onRipe(apple: Apple): Promise<boolean> {
+    if (apple.sweetness !== Sweetness.Sweet)
+        return true;
+
     console.log(`woah I'm a sweet apple!`);
     return true;
 }
